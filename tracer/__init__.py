94c01d568e2a0313f165be405c656519428de227
Hugo Cachitas <hcachitas@gmail.com>
2016-11-03 13:57:52 +0000

    .

--- tracer/__init__.py
+++ tracer/__init__.py
@@ -1,79 +1,8 @@
-from collections import namedtuple
-
-import imageio
-from skimage import exposure
-import pylab
-
-
-class Video:
-
-    def __init__(self, filename):
-        self.filename = filename
-        self.reader = imageio.get_reader(filename, format='FFMPEG', mode='I')
-        self.metadata = self.reader.get_meta_data()
-
-    def __del__(self):
-        self.reader.close()
-        super().__del__()
-        # del self.reader
-
-    def __str__(self):
-        s = "Video:\n"
-        template = "\t{item:14}: {text}\n"
-        s += template.format(item="source", text=self.filename)
-        for key, value in self.metadata.items():
-            s += template.format(item=key, text=value)
-        return s
-
-    @property
-    def fps(self):
-        """Returns the frame rate as a float"""
-        return self.metadata['fps']
-
-    @property
-    def nframes(self):
-        """Returns the number of frames as an int"""
-        return self.metadata['nframes']
-
-    @property
-    def size(self):
-        """Returns a tuple with (width, height)."""
-        return self.metadata['size']
-
-    def read(self, number):
-        """Read a video frame.
-        """
-        if number:
-            data = self.reader.get_data(index=number)
-        else:
-            data = self.reader.get_next_data()
-        return Frame(number, data)
-
-
-class Frame(namedtuple('Frame', ['number', 'image'])):
-
-    __slots__ = ()
-
-    def __str__(self):
-        return "Frame(number=%d)" % self.number
-
-    @property
-    def gray(self):
-        return self.image[:, :, 1]
+import pandas as pd
+import pylab as pl
 
 
 if __name__ == '__main__':
-    filename = 'sample_videos/couple.avi'
-
-    video = Video(filename)
-    print(video)
-
-    f = video.read(0)
-    print(f)
-    print(f.gray)
-    # del f
-
-    # print(video.reader)
-    # video.reader.close()
-    # del video.reader
-    # del video  # without this it hangs randomly on exit
+    s = pd.read_csv('nblobs.csv', index_col=0)
+    s.plot()
+    pl.show()
--- tracer/tools.py
+++ tracer/tools.py
@@ -1,4 +1,64 @@
+from collections import namedtuple
+
+import imageio
 import pylab as pl
+
+
+class Video:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.reader = imageio.get_reader(filename, format='FFMPEG', mode='I')
+        self.metadata = self.reader.get_meta_data()
+
+    def __del__(self):
+        self.reader.close()
+        super().__del__()
+        # del self.reader
+
+    def __str__(self):
+        s = "Video:\n"
+        template = "\t{item:14}: {text}\n"
+        s += template.format(item="source", text=self.filename)
+        for key, value in self.metadata.items():
+            s += template.format(item=key, text=value)
+        return s
+
+    @property
+    def fps(self):
+        """Returns the frame rate as a float"""
+        return self.metadata['fps']
+
+    @property
+    def nframes(self):
+        """Returns the number of frames as an int"""
+        return self.metadata['nframes']
+
+    @property
+    def size(self):
+        """Returns a tuple with (width, height)."""
+        return self.metadata['size']
+
+    def read(self, number):
+        """Read a video frame.
+        """
+        if number:
+            data = self.reader.get_data(index=number)
+        else:
+            data = self.reader.get_next_data()
+        return Frame(number, data)
+
+
+class Frame(namedtuple('Frame', ['number', 'image'])):
+
+    __slots__ = ()
+
+    def __str__(self):
+        return "Frame(number=%d)" % self.number
+
+    @property
+    def gray(self):
+        return self.image[:, :, 1]
 
 
 def imshow(image, title=""):
